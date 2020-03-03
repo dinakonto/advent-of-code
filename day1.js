658042fe2417418775170f95b571ef59c8b96161
Dina Konto <dina.konto@gmail.com>
2020-03-03 21:25:00 +1100

    Added 'to do' comments to complete the challenge

--- day1.js
+++ day1.js
@@ -1,3 +1,11 @@
+/* TO DO
+ * Add an array housing all mass inputs.
+ * Create a loop that
+ *    - Cycles through the array
+ *    - runs calcFuel for each number
+ *    - adds the output of each calcFuel together
+ */
+
 function calcFuel(mass) {
   var fuel = Math.floor(mass / 3) - 2;
   return fuel;
