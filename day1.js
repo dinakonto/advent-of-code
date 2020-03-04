402083f736f2b0458926507546a11a242885eb7d
Dina Konto <dina.konto@gmail.com>
2020-03-04 14:43:21 +1100

    Pt2 steps with console.log checks for single value

--- day1.js
+++ day1.js
@@ -18,7 +18,9 @@
  */
 
 var totalFuel = 0;
-var moduleMass = [78207,89869,145449,73634,78681,81375,131482,126998,50801,115839,77949,53203,146099,56912,59925,132631,115087,89543,123234,108110,109873,81923,124264,87981,106554,147239,73615,72609,129684,84175,64915,98124,74391,55211,120961,119116,148275,89605,115986,120547,50299,137922,78906,145216,80424,122610,61408,97573,127533,116820,76068,77400,117943,85231,102442,62002,58761,56479,98200,85971,73985,88908,82719,120604,83378,88241,122574,76731,99810,137548,102617,105352,137585,83238,118817,149419,107629,6893,56049,70693,83844,76413,87021,90259,124289,102527,139625,106607,120241,101098,66142,96591,82277,142297,116671,131881,94861,79741,73561,115214]
+var moduleMass = [ 100756
+  // 78207,89869,145449,73634,78681,81375,131482,126998,50801,115839,77949,53203,146099,56912,59925,132631,115087,89543,123234,108110,109873,81923,124264,87981,106554,147239,73615,72609,129684,84175,64915,98124,74391,55211,120961,119116,148275,89605,115986,120547,50299,137922,78906,145216,80424,122610,61408,97573,127533,116820,76068,77400,117943,85231,102442,62002,58761,56479,98200,85971,73985,88908,82719,120604,83378,88241,122574,76731,99810,137548,102617,105352,137585,83238,118817,149419,107629,6893,56049,70693,83844,76413,87021,90259,124289,102527,139625,106607,120241,101098,66142,96591,82277,142297,116671,131881,94861,79741,73561,115214
+]
 
 // Function to run the calculation on a single input
 function calcFuel(mass) {
@@ -26,23 +28,38 @@ function calcFuel(mass) {
   return fuel;
 }
 
-// STEPS FOR PART 1
-// Loop through the array
-for (var x = 0; x < moduleMass.length; x++) {
-  // Run calcFuel for each number
-  var singleCalc = calcFuel(moduleMass[x]);
-  // Add this result to the total
-  totalFuel += singleCalc;
-}
+// // STEPS FOR PART 1
+// // Loop through the array
+// for (var x = 0; x < moduleMass.length; x++) {
+//   // Run calcFuel for each number
+//   var singleCalc = calcFuel(moduleMass[x]);
+//   // Add this result to the total
+//   totalFuel += singleCalc;
+// }
+// console.log(totalFuel);
+
+
 
 // STEPS FOR PART 2
-
+console.log("totalFuel = " + totalFuel);
 // for each module moduleMass
-  // calculate its fuel
-
+for (var x = 0; x < moduleMass.length; x++) {
+  // calcFuel on the moduleMass
+  var singleCalc = calcFuel(moduleMass[x]);
+  console.log("singleCalc = " + singleCalc);
+  // add that singleCalc to the total
+  totalFuel += singleCalc;
+  console.log("totalFuel now = " + totalFuel);
   //LOOP:
+  // if result of calcFuel on input >0
+  while (calcFuel(singleCalc) > 0) {
+    console.log("singleCalc " + singleCalc + " is greater than zero")
     // add result to the total
-    // calculate result's fuel
-      // if result is >0
-
-      // else break
+    totalFuel += calcFuel(singleCalc);
+    console.log("totalFuel now = " + totalFuel);
+    // result then becomes the new input
+    singleCalc = calcFuel(singleCalc);
+    console.log("singleCalc now = " + singleCalc);
+  };
+};
+console.log("Total fuel is: " + totalFuel);
