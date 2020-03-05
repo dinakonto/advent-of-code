63502a14178b933ebcba278faee6e587ad7ff9f8
Dina Konto <dina.konto@gmail.com>
2020-03-05 15:16:46 +1100

    Wrote `for` & `if/else` that jumps thru inputs OK

--- 2019-day-2/day2.js
+++ 2019-day-2/day2.js
@@ -7,16 +7,16 @@
 // ULTIMATE QUESTION:
 // What value is left at position 0 after the program halts?
 
-// Creat an array to hold the Intcode program
+// Create an array to hold the intCode program
 let intCode = [
   // REAL VALUES
   // 1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,10,1,19,1,6,19,23,1,23,13,27,2,6,27,31,1,5,31,35,2,10,35,39,1,6,39,43,1,13,43,47,2,47,6,51,1,51,5,55,1,55,6,59,2,59,10,63,1,63,6,67,2,67,10,71,1,71,9,75,2,75,10,79,1,79,5,83,2,10,83,87,1,87,6,91,2,9,91,95,1,95,5,99,1,5,99,103,1,103,10,107,1,9,107,111,1,6,111,115,1,115,5,119,1,10,119,123,2,6,123,127,2,127,6,131,1,131,2,135,1,10,135,0,99,2,0,14,0
-
+1,8,4,5,2,5,7,9,99,6,4,3,5,6,4,7,9,6,6,3,3
   // TEST VALUES AND FINAL STATES FOR TESTING
         // 1,0,0,0,99
         // becomes 2,0,0,0,99 (1 + 1 = 2).
         //
-        2,3,0,3,99
+        // 2,3,0,3,99
         // becomes 2,3,0,6,99 (3 * 2 = 6).
         //
         // 2,4,4,5,99,0
@@ -24,7 +24,30 @@ let intCode = [
         //
         // 1,1,1,4,99,5,6,0,99
         // becomes 30,1,1,4,2,5,6,0,99.
-]
+];
 
-// Print the full program for testing
-console.log(intCode);
+
+// `a` = the index of the Opcode. It jumps 4 each loop.
+for( let a = 0; a < intCode.length; a += 4) {
+  // If the Opcode is 1
+  if (intCode[a] === 1) {
+    // Do this
+    console.log(`${intCode[a]} is the first Opcode`);
+  }
+  //If the Opcode is 2
+  else if (intCode[a] === 2) {
+    // Do this
+    console.log(`${intCode[a]} is the second Opcode`);
+  }
+  // If the Opcode is 99
+  else if (intCode[a] === 99) {
+    // Do this
+    console.log(`${intCode[a]} is the third Opcode`);
+  }
+  // If it's something else weird
+  else {
+    // Do this
+    console.log('Something went wrong');
+  }
+
+}
