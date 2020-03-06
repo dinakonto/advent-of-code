435b96f9dd34850693fc76bd6f8084410d3fc7d6
Dina Konto <dina.konto@gmail.com>
2020-03-06 13:45:05 +1100

    Fixed issues. Now returns correct answer for Pt1!
    Two main issues were:
    1. Missing 'else' in the second condition.
       Was 'if', is now 'else if'.
       This stopped the second condition from running unnecessarily.
    2. Completely missed part of the problem statement:
       "...before running the program, replace position 1 with the
       value 12 and replace position 2 with the value 2."
       Added code at line 39 to enact this change.

--- 2019-day-2/day2.js
+++ 2019-day-2/day2.js
@@ -12,9 +12,10 @@
 // Could be conflict between x values for counting vs displaying value? idk.
 
 // Create an array to hold the intCode program
-const intCode = [
+let intCode = [
   // REAL VALUES
-  // 1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,10,1,19,1,6,19,23,1,23,13,27,2,6,27,31,1,5,31,35,2,10,35,39,1,6,39,43,1,13,43,47,2,47,6,51,1,51,5,55,1,55,6,59,2,59,10,63,1,63,6,67,2,67,10,71,1,71,9,75,2,75,10,79,1,79,5,83,2,10,83,87,1,87,6,91,2,9,91,95,1,95,5,99,1,5,99,103,1,103,10,107,1,9,107,111,1,6,111,115,1,115,5,119,1,10,119,123,2,6,123,127,2,127,6,131,1,131,2,135,1,10,135,0,99,2,0,14,0
+  1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,10,1,19,1,6,19,23,1,23,13,27,2,6,27,31,1,5,31,35,2,10,35,39,1,6,39,43,1,13,43,47,2,47,6,51,1,51,5,55,1,55,6,59,2,59,10,63,1,63,6,67,2,67,10,71,1,71,9,75,2,75,10,79,1,79,5,83,2,10,83,87,1,87,6,91,2,9,91,95,1,95,5,99,1,5,99,103,1,103,10,107,1,9,107,111,1,6,111,115,1,115,5,119,1,10,119,123,2,6,123,127,2,127,6,131,1,131,2,135,1,10,135,0,99,2,0,14,0
+
 
   // TEST VALUES
                    // 1,0,0,0,99
@@ -28,22 +29,18 @@ const intCode = [
         //
                     // 1,1,1,4,99,5,6,0,99
         // becomes 30,1,1,4,2,5,6,0,99.
-                      1,9,10, 3,2,3,11,0,99,30,40,50
+                      // 1,9,10, 3,2,3,11,0,99,30,40,50
         // becomes 3500,9,10,70,2,3,11,0,99,30,40,50
 ];
 
-let resultCode = [...intCode];
+let sum = 0;
 
-// console.log(
-// `Intcode starts as:
-// ${intCode}
-// --------------------
-// `
-// );
+// COMMENT OUT THE FOLLOWING LINE before running test values
+intCode.splice(1,1,12) + intCode.splice(2,1,2);
+
 
 // `i` = the index of the Opcode. It jumps 4 each loop.
 for( let i = 0; i < intCode.length; i += 4) {
-  let sum = 0;
   // console.log(`sum = ${sum}`);
   // console.log(`intCode = ${intCode}`)
   // console.log(`resultCode = ${resultCode}`)
@@ -59,18 +56,17 @@ for( let i = 0; i < intCode.length; i += 4) {
 // and put the result at index ${intCode[x+3]}.
 // `);
     sum = intCode[intCode[x+1]] + intCode[intCode[x+2]];
-    resultCode.splice(resultCode[x+3], 1, sum);
+    intCode.splice(intCode[x+3], 1, sum);
 
     console.log(
-`Was ${intCode}
-Now ${resultCode}`);
+`Now ${intCode}`);
 
 //     console.log(`Initial code read: ${intCode}
 // Result reads: ${resultCode}`);
   }
 
   // If the Opcode is 2
-  if (intCode[i] === 2) {
+  else if (intCode[i] === 2) {
     let x = i;
     console.log(`---> Running Opcode 2`);
 
@@ -88,15 +84,14 @@ Now ${resultCode}`);
     sum = intCode[intCode[x+1]] * intCode[intCode[x+2]];
     // console.log(`Result is: ${sum}`);
     // And put the result in the right spot
-    resultCode.splice(resultCode[x+3], 1, sum);
+    intCode.splice(intCode[x+3], 1, sum);
     // console.log(`Program reads: ${resultCode}
       // `);
 
       console.log(
-  `Was ${intCode}
-  Now ${resultCode}`);
+  `Now ${intCode}`);
 
-  
+
   }
 
   // If the Opcode is 99
@@ -105,8 +100,8 @@ Now ${resultCode}`);
     let x = i;
     console.log(`---> Running Opcode 99`);
     console.log(
-`Was ${intCode}
-Now ${resultCode}`);
+`FINAL RESULT
+Now ${intCode}`);
 //     console.log(
 // `--------------------
 // Running Opcode ${intCode[x]}
