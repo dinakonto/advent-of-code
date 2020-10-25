ba1a68ea6ca6ee252b32f842869dc71969761f1b
Dina Konto <dina.konto@gmail.com>
2020-10-25 19:30:51 +1100

    Refactor to use ES6 syntax and separate functions

--- 2019-day-1/day1.js
+++ 2019-day-1/day1.js
@@ -4,48 +4,50 @@
  *
  */
 
-var totalPartOne = 0;
-var totalPartTwo = 0;
-// Load the elements in moduleMass array from a text file.
-var fs = require('fs');
-var content = fs.readFileSync('inputs.txt','utf-8');
-var moduleMass = content.split('\n');
+let totalPartOne = 0;
+let totalPartTwo = 0;
 
-// Function to run the calculation on a single input
-function calcFuel(mass) {
-  var fuel = Math.floor(mass / 3) - 2;
+// Load the elements in moduleMass array from a text file.
+const fs = require('fs');
+const content = fs.readFileSync('inputs.txt','utf-8');
+const moduleMass = content.split('\n');
+
+// Calculate how much fuel is required to carry a given mass
+function fuelCalculation(mass) {
+  let fuel = Math.floor(mass / 3) - 2;
   return fuel;
 }
 
-// STEPS FOR PART 1
-// For each element in the moduleMass array
-for (var x = 0; x < moduleMass.length; x++) {
-  // Run calcFuel on it
-  var singleCalc = calcFuel(moduleMass[x]);
-  // Add the result to totalFuel
-  totalPartOne += singleCalc;
+// PART ONE
+// What is the sum of the fuel requirements for all of the modules
+// on your spacecraft (inputs.txt)?
+function solvePartOne() {
+  moduleMass.forEach(mass => {
+    let thisCalc = fuelCalculation(mass);
+    totalPartOne += thisCalc;
+  })
+  console.log('PART ONE: Fuel required for modules alone = ' + totalPartOne);
+  return totalPartOne;
 }
 
-// STEPS FOR PART 2
-// for each element in the moduleMass array
-for (var x = 0; x < moduleMass.length; x++) {
-  // calcFuel on the moduleMass
-  var moduleFuel = calcFuel(moduleMass[x]);
-  // add that moduleFuel to the totalFuel
-  totalPartTwo += moduleFuel;
-  // create a new variable to calculate the extraFuel needed for the moduleFuel
-  var extraFuel = moduleFuel;
-  //LOOP:
-  // if result of calcFuel on input >0
-  while (calcFuel(extraFuel) > 0) {
-    // add result to the total
-    totalPartTwo += calcFuel(extraFuel);
-    // result then becomes the new input
-    extraFuel = calcFuel(extraFuel);
-  };
-};
+// PART TWO
+// What is the sum of the fuel requirements for all of the modules on your
+// spacecraft when also taking into account the mass of the fuel required
+// to carry the fuel itself?
+function solvePartTwo() {
+  moduleMass.forEach(mass => {
+    let moduleFuel = fuelCalculation(mass);
+    totalPartTwo += moduleFuel;
+    let extraFuel = moduleFuel;
+    while (fuelCalculation(extraFuel) > 0) {
+      totalPartTwo += fuelCalculation(extraFuel);
+      extraFuel = fuelCalculation(extraFuel);
+    }
+  })
+  console.log('PART TWO: Total fuel required is = ' + totalPartTwo);
+  return totalPartTwo;
+}
 
-// Printing results to the console
-console.log("Fuel required for modules alone: " + totalPartOne + "\n" +
-            "Extra fuel required to carry that fuel: " + (totalPartTwo-totalPartOne) + "\n" +
-            "Total fuel required is: " + totalPartTwo);
+// RUN SOLUTION
+solvePartOne();
+solvePartTwo();
